5e924346e336d88327e18c5487721afa2822ecab
Tesfa Yohannes <tmyohannes@gmail.com>
2019-02-28 11:52:22 +0300

    Map to mainnet port

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo docker build -t singularitynet/edge-detection-service .
-sudo docker run -d -v /home/zelalem/pytorch-hed/etcd:/pytorch-hed/etcd -v /etc/letsencrypt:/etc/letsencrypt -it -p 8001:8001 -p 8002:8002 --name edge-detection-service singularitynet/edge-detection-service
+sudo docker run -d -v /home/zelalem/pytorch-hed/etcd:/pytorch-hed/etcd -v /etc/letsencrypt:/etc/letsencrypt -it -p 8012:8012 -p 8002:8002 --name edge-detection-service singularitynet/edge-detection-service
